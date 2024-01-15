ffe04b60283b8364f779b5e32f357ea9fcf54fc2
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-01-15 20:15:06 +0100

    parte del 7

---
+++ IdeaProyects/Relación 1 POO/Ejercicio7/Incidencia.java
@@ -0,0 +1,16 @@
+public class Incidencia {
+    enum Estado {ABIERTA, ENPROCESO, RESUELTA}
+    private String codigo;
+    private Estado estado;
+    private String problema;
+    private String solucion;
+
+    public static int pendientes = 0;
+
+public Incidencia(String codigo, String problema, String solucion) {
+    this.codigo = codigo;
+    this.estado = Estado.ABIERTA;
+    this.problema = problema;
+    this.solucion = solucion;
+}
+}
---
+++ IdeaProyects/Relación 1 POO/Ejercicio7/TestIncidencia.java
@@ -0,0 +1,14 @@
+public class TestIncidencia {
+    
+    public static void main(String[] args) {
+        Incidencia inc1 = new Incidencia("I001", "Farola rota en la calle noseque", null);
+        Incidencia inc2 = new Incidencia("I001", "tubería rota en calle noseque", null);
+        Incidencia inc3 = new Incidencia("I001", "carretera a asfaltar en calle noseque", null);
+
+        System.out.println(Incidencia.pendientes);
+        System.out.println(inc1);
+        System.out.println(inc2);
+    }
+
+        
+}
