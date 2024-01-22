51dafe60e309eb2ae7758c87dda8ac7a869b3fea
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-01-22 21:40:36 +0100

    test ejercicio 3

--- IdeaProyects/Relacion 1 POO/src/ejercicio3/test.java
+++ IdeaProyects/Relacion 1 POO/src/ejercicio3/test.java
@@ -5,7 +5,7 @@ public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Pajaro p1 = new Pajaro();
-        int edad = p1.setedad(Integer.parseInt(sc.nextLine());
+        int edad = p1.setedad(Integer.parseInt(sc.nextLine()));
         String impreso = p1.printedad();
 
     }
