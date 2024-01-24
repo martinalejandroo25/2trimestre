82fbf53c208cad2d10d6c066dfb8dd76435279af
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-01-24 05:18:07 +0100

    Proyecto de Gradle en terminal SCRUM

---
+++ Las demas asignaturas/Entornos/ProyectoGradleTerminal/app/src/main/java/com/mgh/edd/proyectogradleterminal/app/Operaciones.java
@@ -0,0 +1,12 @@
+package com.mgh.edd.proyectogradleterminal.app;
+
+public class Operaciones {
+    public  int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
+
