b0316ba614c5decd30c9d75dc885e0d63d1f17c7
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-01-24 05:18:23 +0100

    proyecto de Gradle en terminal SCRUM

--- IdeaProyects/Relacion 1 POO/src/EjerciciosPropuestos/Producto.java
+++ IdeaProyects/Relacion 1 POO/src/EjerciciosPropuestos/Producto.java
@@ -1,4 +1,7 @@
 package EjerciciosPropuestos;
+
+import java.util.Objects;
+
 /*Producto {
     String nombre;
     double precio;
@@ -10,5 +13,50 @@ package EjerciciosPropuestos;
     equals(nombre)
 }*/
 public class Producto {
+    private String nombre;
+    private double precio;
 
+    public Producto(String nombre) {
+
+    }
+    public Producto(String nombre, double precio) {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Producto{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", precio=").append(precio);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Producto producto)) return false;
+        return Double.compare(precio, producto.precio) == 0 && Objects.equals(nombre, producto.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, precio);
+    }
 }
--- IdeaProyects/Relacion 1 POO/src/EjerciciosPropuestos/Tienda.java
+++ IdeaProyects/Relacion 1 POO/src/EjerciciosPropuestos/Tienda.java
@@ -1,4 +1,8 @@
 package EjerciciosPropuestos;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
 /*Tienda{
     String nombre;
     ArrayList<Producto> productos;
@@ -9,5 +13,50 @@ package EjerciciosPropuestos;
     equals(nombre)
     addProducto(producto prod)
 }*/
-public class Tienda {
+public abstract class Tienda {
+    private String nombre;
+    private ArrayList<Producto> productos;
+
+    public Tienda(String nombre) {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(ArrayList<Producto> productos) {
+        this.productos = productos;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Tienda{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", productos=").append(productos);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tienda tienda)) return false;
+        return Objects.equals(nombre, tienda.nombre) && Objects.equals(productos, tienda.productos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, productos);
+    }
+    Producto p1 = new Producto();
+    add.producto(p1 prod);
 }
