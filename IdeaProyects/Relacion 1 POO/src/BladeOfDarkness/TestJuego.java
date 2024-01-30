b8a061a6e8f924a6bfe54da4f4b51f67e372c1cb
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-01-30 04:46:40 +0100

    Blade of Darkness casi completado

---
+++ IdeaProyects/Relacion 1 POO/src/BladeOfDarkness/TestJuego.java
@@ -0,0 +1,7 @@
+package BladeOfDarkness;
+
+public class TestJuego {
+    Jugador j1 = new Jugador("Martín", 3, 5, 500,
+            null, null, espada1);
+    Arma espada1 = new Arma("EspadaNV1", Arma.tipo.ESPADA, 80, true);
+}
