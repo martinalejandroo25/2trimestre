179072d3356ae543695e51fa5a7d0b714c0b4d8a
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-02-01 21:54:31 +0100

    Lenguaje de marcas

---
+++ Las demas asignaturas/Sistemas Informáticos/PruebasBash/menu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#    3. Crea un menú para indicar:
+#        1. Cuántos usuarios están conectados
+#        2. Usuarios con directorio home y 
+#           almacenarlos  en un fichero llamado all.users
+#        3. Lista de cuentas de usuarios ordenados 
+#            por nombre y por id
+#        4. Día mes y año mostradas en pantalla 
+#        con el formato dia-mes-año
+#        5. Cambiar a mayúsculas los nombres de 
+#            los ficheros pasados por parámetros.
