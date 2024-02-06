b1ae2d0b9fb31e430056abd1f40838bc08157015
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-02-06 19:26:21 +0100

    relacion 1 completa y relacion 2 parcial SQL BASES DE DATOS

--- Las demas asignaturas/Bases de Datos/practicaSQL/Relacion1.sql
+++ Las demas asignaturas/Bases de Datos/practicaSQL/Relacion1.sql
@@ -51,29 +51,50 @@ select * from alumno;
 #3.- Mostrar todos los nombres de los alumnos con sus teléfonos.
 select nom_al, nom_prof from alumno,profesor,relacion where relacion.id_al=alumno.id_al;
 
-select nom_al as alumno, nom_prof as profesor from alumno as a, profesor a /*me quede a medias aqui*/
+/*select nom_al as alumno, nom_prof as profesor from alumno as a, profesor a;*//*me quede a medias aqui*/
 #4.- Mostrar los nombres de los alumnos ordenados en orden creciente y decreciente.
-
+select nom_al from alumno order by nom_al asc; /*ascendente*/
+select nom_al from alumno order by nom_al desc; /*descendente*/
 #5.- Mostrar los alumnos ordenados por edad.
-
+select nom_al from alumno order by fech_al asc;
 #6.- Mostrar nombre de alumnos que contengan alguna ‘A’ en el nombre.
-
+select nom_al from alumno where nom_al like "%A%"; /*A% AL PRINCIPIO %A AL FINAL  %A% CONTIENE*/
 #7.- Mostrar id_al ordenado por nota.
-
+select id_al from relacion order by nota; 
 #8.- Mostrar nombre alumno y nombre de sus respectivos profesores.
-
+select alumno.nom_al, profesor.nom_prof from alumno
+join relacion on alumno.id_al = relacion.id_al
+join profesor on relacion.id_prof = profesor.id_prof;
 #9.- Mostrar el nombre de los alumnos que les de clase el profesor P01
-
+select alumno.nom_al from alumno
+join relacion on alumno.id_al = relacion.id_al
+join profesor on relacion.id_prof = profesor.id_prof where profesor.id_prof = "P01";
 #10.- Mostrar el nombre y la nota de los alumnos que les de clase el profesor ‘FERNAND0 GARCIA’.
-
+select alumno.nom_al, relacion.nota from alumno 
+join relacion on alumno.id_al = relacion.id_al
+join profesor on relacion.id_prof = profesor.id_prof where profesor.nom_prof = "FERNANDO GARCIA";
 #11.- Mostrar todos los alumnos (codigo) que hayan aprobado con el profesor P01.
-
+select alumno.id_al, relacion.nota from alumno
+join relacion on alumno.id_al = relacion.id_al
+join profesor on relacion.id_prof = profesor.id_prof where profesor.id_prof = "P01" and relacion.nota >= 5;
 #12.- Mostrar todos los alumnos (nombre) que hayan aprobado con el profesor P01.
-
+select alumno.nom_al, relacion.nota from alumno
+join relacion on alumno.id_al = relacion.id_al
+join profesor on profesor.id_prof = relacion.id_prof where profesor.id_prof = "P01" and relacion.nota >= 5;
 #13.- Mostrar todos los alumnos (nombre) que hayan aprobado con el profesor ‘CARMEN TORRES’.
-
+select alumno.nom_al, relacion.nota from alumno
+join relacion on alumno.id_al = relacion.id_al
+join profesor on profesor.id_prof = relacion.id_prof where profesor.nom_prof = "CARMEN TORRES" and relacion.nota >= 5;
 #14.- Mostrar el alumno/s que haya obtenido la nota más alta con ‘P01’,
+select alumno.nom_al, relacion.nota from alumno
+join relacion on alumno.id_al = relacion.id_al
+join profesor on profesor.id_prof = relacion.id_prof 
+where relacion.nota = (select max(nota) from relacion where relacion.id_prof = "P01" );
+#15.- Mostrar los alumnos (nombre y codigo) que hayan aprobado todo.
+select alumno.nom_al, alumno.id_al from alumno
+where alumno.id_al not in (select relacion.id_al from relacion where relacion.nota < 5);
 
-#15.- Mostrar los alumnos (nombre y codigo) que hayan aprobado todo. 
+/*select relacion.nota, alumno.nom_al from alumno
+join relacion on alumno.id_al = relacion.id_al;*/
 
 
--- Las demas asignaturas/Bases de Datos/practicaSQL/Relacion2.sql
+++ Las demas asignaturas/Bases de Datos/practicaSQL/Relacion2.sql
@@ -7,13 +7,13 @@ Nombre Varchar(30)
 
 create table if not exists articulos(
 Clave_articulo Int primary key,
-Nombre Varchar(30),
-Precio Int,
-Clave_fabricante Int,
+nombre Varchar(30),
+nrecio Int,
+clave_fabricante Int,
 foreign key (Clave_fabricante) references fabricantes(Clave_fabricante)
 on delete cascade on update cascade
 ) engine=innodb;
-
+alter table articulos change nrecio  precio  int;
 show tables;
 describe articulos;
 
@@ -35,4 +35,53 @@ insert into articulos values
 (8, "DVD Rom", 450, 2),
 (9, "CD Rom", 200, 2),
 (10, "Tarjeta de red", 180, 3);
+
+# 8. Genera las siguientes consultas:
+# a) Obtener todos los datos de los productos de la tienda
 select * from articulos;
+# b) Obtener los nombres de los productos de la tienda
+select nombre from articulos;
+# c) Obtener los nombres y precio de los productos de la tienda
+select nombre, precio from articulos;
+# d) Obtener los nombres de los artículos sin repeticiones
+select distinct nombre from articulos; /*distinct para tablas con informacion repetida*/
+# e) Obtener todos los datos del artículo cuya clave de producto es ‘5’
+select * from articulos where Clave_articulo = 5;
+# f) Obtener todos los datos del artículo cuyo nombre del producto es ‘’Teclado”
+select * from articulos where nombre like "Teclado";
+# g) Obtener todos los datos de la Memoria RAM y memorias USB
+select * from articulos where nombre like "Memoria RAM" OR nombre like "Memoria USB";
+# h) Obtener todos los datos de los artículos que empiezan con ‘M’
+select * from articulos where nombre like "M%";
+# i) Obtener el nombre de los productos donde el precio sea $ 100
+select nombre from articulos where precio = 100;
+# j) Obtener el nombre de los productos donde el precio sea mayor a $ 200
+select nombre from articulos where precio > 200;
+# k) Obtener todos los datos de los artículos cuyo precio este entre $100 y $350
+select * from articulos where precio between 100 and 350; /*between entre 1 valor "and" y el otro valor*/
+# l) Obtener el precio medio de todos los productos
+select avg(precio) from articulos;
+# m) Obtener el precio medio de los artículos cuyo código de fabricante sea 2
+select avg(precio) from articulos where clave_fabricante = "2";
+# n) Obtener el nombre y precio de los artículos ordenados por Nombre
+select nombre, precio from articulos order by nombre;
+# o) Obtener todos los datos de los productos ordenados descendentemente por Precio
+select * from articulos order by precio desc;
+# p) Obtener el nombre y precio de los artículos cuyo precio sea mayor a $ 250 y ordenarlos descendentemente por precio
+#    y luego ascendentemente por nombre
+select nombre, precio from articulos where precio > 250 order by precio desc;
+select nombre, precio from articulos where precio > 250 order by nombre asc;
+# q) Obtener un listado completo de los productos, incluyendo por cada articulo los datos del articulo y del fabricante
+select articulos.*, fabricante.* from articulos
+join articulos on articulos.clave_fabricante = fabricante.Clave_fabricante
+join fabricante on fabricante.Clave_fabricante = articulos.clave_fabricante;
+# r) Obtener la clave de producto, nombre del producto y nombre del fabricante de todos los productos en venta
+# s) Obtener el nombre y precio de los artículos donde el fabricante sea Logitech ordenarlos alfabéticamente por nombre
+#    del producto
+# t) Obtener el nombre, precio y nombre de fabricante de los productos que son marca Lexar o Kingston ordenados
+#    descendentemente por precio
+# u) Añade un nuevo producto: Clave del producto 11, Altavoces de $ 120 del fabricante 2
+# v) Cambia el nombre del producto 6 a ‘Impresora Laser’
+# w) Aplicar un descuento del 10% a todos los productos.
+# x) Aplicar un descuento de $ 10 a todos los productos cuyo precio sea mayor o igual a $ 300
+# y) Borra el producto numero 6
