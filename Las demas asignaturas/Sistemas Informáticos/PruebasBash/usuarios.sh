d3e98d0f9eecbcbdc88fac3c60d767f6c726dbe1
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-02-06 23:45:19 -0500

    comentarios en bash

--- Las demas asignaturas/Sistemas Informáticos/PruebasBash/usuarios.sh
+++ Las demas asignaturas/Sistemas Informáticos/PruebasBash/usuarios.sh
@@ -1,24 +1,30 @@
+#!/bin/bash
+#Dar informacion de un usuario en bash
 Informacion() {
 
 }
+#Eliminar un usuario en bash
 deleteUser() {
 
 }
-
+#Añadir un usuario en bash
 addUsuarios() {
 
 }
-
+#Añadir un usuario a un grupo en bash
 addUserToGroup() {
 
 }
+#Eliminar un usuario de un grupo en bash
 removeUserFromGroup() {
 
 }
-addgroup() {
+#Eliminar un grupo en bash
+deleteGroup() {
 
 }
-deleteGroup(){
+#Añadir un grupo en bash
+addgroup() {
 
 }
 
