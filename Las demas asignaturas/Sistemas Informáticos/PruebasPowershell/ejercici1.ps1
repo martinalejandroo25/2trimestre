d4116d7b13ff6032e411750769498c66fcdf9d94
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-02-14 18:28:07 +0100

    POO Ejercicio Evaluable envíos productos

---
+++ Las demas asignaturas/Sistemas Informáticos/PruebasPowershell/ejercici1.ps1
@@ -0,0 +1,17 @@
+﻿#Crea un SCRIPT que muestre un menú al usuario con las siguientes operaciones:
+#1. Mostrar directorio.
+#2. Crear directorio.
+#3. Eliminar directorio.
+#4. Cambiar a un directorio, ver su contenido y volver al directorio anterior.
+#5. Salir.
+#Una vez elegida una opción correcta el programa debe solicitar un nombre de directorio y hacer la
+#operación seleccionada.
+#El menú debe repetirse mientras no se pulse la opción de salir.
+#Cada operación debe realizarse en una función independiente.
+function menu {
+    write "1. Mostrar Directorio"
+    write "2. Crear Directorio"
+    write "3. Eliminar Directorio"
+    write 4. Cambiar a un directorio, ver su contenido y volver al directorio anterior
+    write 5. Salir
+}
