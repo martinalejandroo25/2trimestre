0dbd995882cacaa99e48cf114af3f245c183bc83
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-02-21 21:19:09 +0100

    muchas cosas, examen ut3 entornos tambien

---
+++ Las demas asignaturas/Entornos/Refactorización/EXAMEN UT3/RefacTablero2/src/Simbolos.java
@@ -0,0 +1,39 @@
+package RefacTablero2.src;
+
+public final class Simbolos {
+
+    /* TABLERO */
+    // Lí­neas de la parte de ARRIBA
+    static final String ARRIBA_IZQUIERDA = "\u250C";
+    static final String ARRIBA = "\u252C";
+    static final String ARRIBA_DERECHA = "\u2510";
+    // Lí­neas intermedias
+    static final String HORIZONTAL = "\u2500";
+    static final String VERTICAL = "\u2502";
+    static final String IZQUIERDA = "\u251C";
+    static final String CENTRO = "\u253C";
+    static final String DERECHA = "\u2524";
+    // Lí­neas de la parte de ABAJO
+    static final String ABAJO_IZQUIERDA = "\u2514";
+    static final String ABAJO = "\u2534";
+    static final String ABAJO_DERECHA = "\u2518";
+
+    /* CASILLAS */
+    static final String CASILLA_NEGRA = "\u2588";
+    static final String CASILLA_BLANCA = "\u2591";
+    static final String ESPACIO_EN_BLANCO = "\u0020";
+
+    /* PIEZAS */
+    static final String REY_BLANCAS = "\u2654";
+    static final String REY_NEGRAS = "\u265A";
+    static final String REINA_BLANCAS = "\u2655";
+    static final String REINA_NEGRAS = "\u265B";
+    static final String TORRE_BLANCAS = "\u2656";
+    static final String TORRE_NEGRAS = "\u265C";
+    static final String ALFIL_BLANCAS = "\u2657";
+    static final String ALFIL_NEGRAS = "\u265D";
+    static final String CABALLO_BLANCAS = "\u2658";
+    static final String CABALLO_NEGRAS = "\u265E";
+    static final String PEON_BLANCAS = "\u2659";
+    static final String PEON_NEGRAS = "\u265F";
+}
